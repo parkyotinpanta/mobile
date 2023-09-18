d261ea983e1c2fb9092ed9dd341acfeee1c154ca
parkyotinpanta <khwansingz@hotmail.com>
2023-09-18 16:21:15 +0700

    เปลี่ยนนิดหน่อย

--- models/service.mobile.js
+++ models/service.mobile.js
@@ -1,14 +1,19 @@
 const knex = require('../DATA/DataKnex')
 
-module.exports = {
-    select: (req) => knex.select(req.query.filter).from('phone'),
-    insert: (req) => knex('phone').insert(req.body),
-    updete: (req) => knex('phone').where({ id:req.params.id }).updete(req.body),
-    delete: (req) => knex('phone').where({ id:req.params.id }).delete()
-     }
-
-
-
+const seclet =((req)=>{
+    return knex.select(req.query.filter).from('phone')
+})
+const insert =((req)=>{
+    return knex('phone').insert(req.body)
+})
+const updete = ((req)=>{
+    return knex('phone').where({ id:req.params.id }).updete(req.body)
+})
+const remove = ((req)=>{
+    return knex('phone').where({ id:req.params.id }).delete()
+})
+module.exports = {seclet ,insert ,updete ,remove }
+// 1-8 สไตล์การเขียน ดูแอรโลฟังชั่น
 
 
 
